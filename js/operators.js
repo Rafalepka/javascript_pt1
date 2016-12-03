22235afedfeb0ba1e505d92add2d170b6342d9f2
Rafal Matalowski <rafal.matalowski@gmail.com>
2016-12-03 10:17:46 +0100

    init

---
+++ js/operators.js
@@ -0,0 +1,40 @@
+"use strict";
+//
+//var liczba1 = 5;
+//var liczba2 = 17;
+//
+//while (liczba2 > liczba1) {
+//    console.log(liczba2);
+//    --liczba2;
+//}
+
+/*if (2>5) { console.log("tak")
+         } else {
+             console.log("nie")
+         }
+         to co ponizej to skrot tego co tu
+         */
+//
+//(2 === 5) ? console.log("tak"): console.log("nie");
+//
+//if (("2" !== 2) || (8 > 4)) {
+//    document.write("<br> prawda");
+//}
+//
+//if ( ("2" !== 2) || (8 > 4) ) {
+//    document.write("<br> prawda");
+//}
+//
+
+
+var liczba3 = 6;
+var liczba4 = 7;
+//
+//if ( liczba4 >= liczba3) {
+//    console.log("tak większe")
+//} else {
+//    console.error("nie mniejsze")
+//}
+
+
+((liczba4 >= liczba3) || (liczba4 == liczba3)) ? console.log("większe"): console.log("mniejsze");
