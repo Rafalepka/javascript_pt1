18067a887c1173948df91512803aefa1664682ff
Rafal Matalowski <rafal.matalowski@gmail.com>
2016-12-03 11:53:02 +0100

    operatory
    i nowe tablice

--- js/variable-scope.js
+++ js/variable-scope.js
@@ -1,1 +1,28 @@
 "use strict";
+
+/*var wart1 = ;
+var wart2 = ;
+
+function costam () {
+    tresc funkcji
+    warunki
+}
+
+wyswietlenie wyniku funkcji */
+
+var liczba1;
+liczba1 = 4;
+function dodawanie () { 
+    var liczba2;
+    liczba2= 6;
+    var wynik = liczba1 + liczba2;
+    document.write(wynik);
+}
+
+dodawanie();
+
+document.write(wynik);
+
+
+
+
